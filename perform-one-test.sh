f375705a23198c85ef6913db2cdc865fb46db286
Tomasz Grabiec <tgrabiec@gmail.com>
2014-05-08 11:08:31 +0200

    Update test parameters to maximize throughput

--- perform-one-test.sh
+++ perform-one-test.sh
@@ -8,12 +8,12 @@ fi
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "=== Warmup ==="
-wrk -t4 -c256 -d30s http://${GUEST_IP}:8081/servlet/json
+wrk -t1 -c128 -d1m http://${GUEST_IP}:8081/servlet/json
 
 echo "=== Main test ==="
 wrk_out_file=wrk.${TIMESTAMP}
 echo "Output goes to $wrk_out_file"
-wrk --latency -t4 -c128 -d1m http://${GUEST_IP}:8081/servlet/json > ${wrk_out_file}
+wrk --latency -t2 -c256 -d1m http://${GUEST_IP}:8081/servlet/json > ${wrk_out_file}
 
 echo "=== Results ==="
 ./wrk-parse.py ${wrk_out_file}
